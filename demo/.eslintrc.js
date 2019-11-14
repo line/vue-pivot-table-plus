cb82d93052644e81ce88a24550ea6be18c3cdb22
SAKAGUCHI TATSUHIKO <tatsuhiko.sakaguchi@linecorp.com>
2019-11-14 14:09:03 +0900

    updated demo app

---
+++ demo/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  'extends': [
+    'plugin:vue/essential',
+    '@vue/standard',
+    'eslint:recommended'
+  ],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    "no-irregular-whitespace": "warn"
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  }
+}
